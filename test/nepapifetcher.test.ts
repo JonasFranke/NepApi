1c9b7eb0045d07f0ec6bd05e6b3adfe63abea330
Jonas Franke <info@jonasfranke.xyz>
2025-01-27 17:11:01 +0100

    Remove dependabot

--- test/nepapifetcher.test.ts
+++ test/nepapifetcher.test.ts
@@ -2,12 +2,12 @@ import { expect, test } from "bun:test";
 import { getJwtToken, getSiteData } from "../src/nepapifetcher";
 
 test("Login function", async () => {
-  const jwt = await getJwtToken(process.env.username, process.env.password);
-  expect(jwt).toBeDefined();
+    const jwt = await getJwtToken(process.env.username, process.env.password);
+    expect(jwt).toBeDefined();
 });
 
 test("API fetcher", async () => {
-  const jwt = await getJwtToken(process.env.username, process.env.password);
-  const siteData = await getSiteData(process.env.sid, jwt);
-  expect(siteData).toBeDefined();
+    const jwt = await getJwtToken(process.env.username, process.env.password);
+    const siteData = await getSiteData(process.env.sid, jwt);
+    expect(siteData).toBeDefined();
 });
