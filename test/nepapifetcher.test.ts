51b2d8bed72520da315f6de58100a264d7f9c6a9
Jonas Franke <info@jonasfranke.xyz>
2025-03-18 08:26:45 +0100

    Add env var present test

--- test/nepapifetcher.test.ts
+++ test/nepapifetcher.test.ts
@@ -1,6 +1,12 @@
 import { expect, test } from "bun:test";
 import { getJwtToken, getSiteData } from "../src/nepapifetcher";
 
+test("Env variables present", () => {
+    expect(process.env.username).toBeDefined();
+    expect(process.env.password).toBeDefined();
+    expect(process.env.sid).toBeDefined();
+});
+
 test("Login function", async () => {
     const jwt = await getJwtToken(process.env.username, process.env.password);
     expect(jwt).toBeDefined();
