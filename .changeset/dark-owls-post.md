09a531ea2455f762dffd045d018610423d7266e5
Jonas Franke <90050350+JonasFranke@users.noreply.github.com>
2025-06-22 13:05:57 +0200

    prevent 0 return midday (#21)

---
+++ .changeset/dark-owls-post.md
@@ -0,0 +1,5 @@
+---
+"nep_api": minor
+---
+
+Prevent reset of data due to endpoint failure
--- src/store.ts
+++ src/store.ts
@@ -8,6 +8,20 @@ let healthy = true;
 let siteData: statisticsProduction | undefined;
 let jwtToken: string | undefined;
 
+class Store {
+  today: number;
+  total: number;
+  totalNow: number;
+
+  constructor(today = 0, total = 0, totalNow = 0) {
+    this.today = today;
+    this.total = total;
+    this.totalNow = totalNow;
+  }
+}
+
+const store = new Store();
+
 export async function getSiteDataWithHealth() {
   try {
     if (!jwtToken) {
@@ -15,6 +29,7 @@ export async function getSiteDataWithHealth() {
     }
     if (!siteData) {
       siteData = await getSiteData(process.env.sid, jwtToken);
+      writeCache(siteData);
       healthy = true;
       console.log("created cache");
       setTimeout(
@@ -30,7 +45,38 @@ export async function getSiteDataWithHealth() {
     console.error(e);
     jwtToken = await getJwtToken(process.env.username, process.env.password);
   }
+
+  if (siteData) {
+    console.log(`store: ${store.today}, ${store.total}, ${store.totalNow}`);
+    console.log(
+      `siteData: ${siteData.today}, ${siteData.total}, ${siteData.totalNow}`,
+    );
+    siteData.today = store.today;
+    siteData.total = store.total;
+    siteData.totalNow = store.totalNow;
+  }
+
   return siteData;
+}
+
+function writeCache(data: statisticsProduction) {
+  if (data.today > store.today) {
+    store.today = data.today;
+    resetStoreTime();
+  }
+  if (data.total > 0) {
+    store.total = data.total;
+  }
+  if (data.totalNow > store.totalNow) {
+    store.totalNow = data.totalNow;
+  }
+}
+
+function resetStoreTime() {
+  const time = new Date().getHours();
+  if (time === 0) {
+    store.today = 0;
+  }
 }
 
 function invalidateCache() {
